29caafb506325dab7b8f0733da411b1ada1962b9
dascard <dascard601@gmail.com>
2024-12-17 16:36:20 +0800

    Merge

--- header/Logic/Building.h
+++ header/Logic/Building.h
@@ -1,22 +1,12 @@
 #pragma once
 #include "Player.h"
 #include <vector>
-<<<<<<< HEAD
-class Building {
-	int level;
-	std::vector<int> cost;
-	std::vector<int> production;
-public:
-	Building();
-	~Building();
-	bool upLevel(Player player);
-=======
 #include <string>
 class Building {
 	int level;
 	std::vector<int> cost;
 
-	//�ƽ�ʯ�͡��ֲġ�����������,Ѳ��,�г�,�޼�,ս��
+	//person, 
 	std::vector<int> production;
 	std::string name;
 public:
@@ -27,5 +17,4 @@ public:
 	std::vector<int> getCost();
 	std::string getName();
 	bool remove(Player& player);
->>>>>>> 342fef05818501dee7608a80ac7adf078302a9c5
 };
