1a344567bd981711f23702456eb896a4b150149f
dascard <dascard601@gmail.com>
2024-12-19 22:16:31 +0800

    add config

--- header/Logic/Building.h
+++ header/Logic/Building.h
@@ -5,11 +5,12 @@
 class Building {
 	int level;
 	//gold,oil,elec,steel,labor,ICBM,MRBM,CM,fighter
+	int labor;
 	std::string name;
 public:
 	Building(std::string);
 	~Building();
-	bool upLevel(int MaxLevel);
+	bool upLevel(int MaxLevel, int labor);
 	std::string getName();
 	bool remove();
 };
--- header/utils/Config.h
+++ header/utils/Config.h
@@ -1,7 +1,20 @@
 #include "../../include/json.hpp"
+#include <initializer_list>
+#include <vector>
 
 using json = nlohmann::json;
 
 class Config {
-	
-}
+	json config;		
+	json tmp;
+	void getKey(const json*& tmp, const std::string& key);
+public:
+	Config(const std::string& path);
+	~Config();
+	/* template<typename T, typename... Args> */
+	/* const json getConfig(const T& first, const Args&... args); */
+	/* template<typename T> */
+	const json getConfig(std::initializer_list<std::string> args);
+	const json getConfig(std::string s);
+	const json& getConfig();
+};
